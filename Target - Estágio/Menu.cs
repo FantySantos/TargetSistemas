918f65b6283e9ae4c5a2cc4258345d1e2c361547
FantySantos <fantiny.santos@gmail.com>
2024-09-05 12:58:01 -0300

    Adicionar arquivos de projeto.

---
+++ Target - Estágio/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Target_Sistemas
+{
+    class Menu
+    {
+        string Linha(int tamanhoLinha = 52) { return new string('-', tamanhoLinha); }
+
+        public void Cabecalho(string textoCabecalho)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(Linha());
+            Console.WriteLine(CentralizarTexto(textoCabecalho));
+            Console.WriteLine(Linha());
+            Console.ResetColor();
+        }
+
+        string CentralizarTexto(string texto, int totalCaracteres = 52)
+        {
+            int espacos = totalCaracteres - texto.Length;
+            int padEsquerdo = espacos / 2;
+            int padDireito = espacos - padEsquerdo;
+
+            return new string(' ', padEsquerdo) + texto + new string(' ', padDireito);
+        }
+
+        public string Main(string[] opcoes)
+        {
+            Cabecalho("MENU PRINCIPAL");
+            for (int i = 0; i < opcoes.Length; i++)
+                Console.WriteLine($"\x1b[93m{i+1}\x1b[39m - \x1b[96m{opcoes[i]}\x1b[39m");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Sua opção: ");
+            Console.ResetColor();
+            var opcao = Console.ReadLine();
+            return opcao;
+        }
+
+    }
+}
---
+++ Target - Estágio/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+
+namespace Target_Sistemas
+{
+    class Target_Sistemas
+    {
+        void PertenceFibonacci(int inputUsuario)
+        {
+
+            if (inputUsuario <= 1)
+            {
+                Console.WriteLine($"O número {inputUsuario} pertence à sequência de Fibonacci.");
+                return;
+            }
+
+
+            int primeiroAntecessor = 1, segundoAntecessor = 0, subsequente;
+
+            while (primeiroAntecessor < inputUsuario)
+            {
+                subsequente = primeiroAntecessor + segundoAntecessor;
+                segundoAntecessor = primeiroAntecessor;
+                primeiroAntecessor = subsequente;
+            }
+
+            if (primeiroAntecessor == inputUsuario)
+            {
+                Console.WriteLine($"O número {primeiroAntecessor} pertence à sequência de Fibonacci.");
+                return;
+            }
+
+            Console.WriteLine($"O número {inputUsuario} não pertence à sequência de Fibonacci.");
+
+        }
+
+        void VerificaLetraA(string inputUsuario)
+        {
+            inputUsuario = inputUsuario.ToLower();
+
+            if (inputUsuario.Contains('a'))
+            {
+                int quantidadeA = inputUsuario.Count(x => x == 'a');
+                Console.WriteLine($"A letra 'a' existe na string e aparece {quantidadeA} {(quantidadeA == 1 ? "vez" : "vezes")}.");
+                return;
+            }
+
+            Console.WriteLine("A letra 'a' não existe na string.");
+        }
+
+        static void Main(string[] args)
+        {
+            Target_Sistemas ts = new Target_Sistemas();
+            Menu menu = new Menu();
+
+            while (true)
+            {
+                menu.Cabecalho("Target Sistemas - Processo Seletivo");
+
+                var opcao = menu.Main(["Verificar se pertence à sequência Fibonacci", "Verificar letra 'a'", "Sair"]);
+                switch (opcao)
+                {
+                    case "1":
+                        Console.Clear();
+                        menu.Cabecalho("Opção 1 - Fibonacci");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Informe um número: ");
+                        Console.ResetColor();
+
+                        if (int.TryParse(Console.ReadLine(), out int inputUsuario))
+                        {
+                            ts.PertenceFibonacci(inputUsuario);
+                            break;
+                        }
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Entrada inválida! Por favor, insira um número válido.");
+                        Console.ResetColor();
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        menu.Cabecalho("Opção 2 - Verificar Letra 'a'");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Informe uma string: ");
+                        Console.ResetColor();
+
+                        ts.VerificaLetraA(Console.ReadLine());
+                        break;
+
+                    case "3":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("Tem certeza de que deseja sair? (S/N): ");
+                        Console.ResetColor();
+
+                        var confirmacao = Console.ReadLine().ToUpper()[0];
+                        Console.WriteLine();
+                        if (confirmacao == 'S')
+                        {
+                            menu.Cabecalho("Saindo... Até logo!");
+                            Thread.Sleep(2000);
+                            return;
+                        }
+
+                        Console.Clear();
+                        continue;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nOpção inválida. Tente novamente.");
+                        Console.ResetColor();
+                        Thread.Sleep(2000);
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+}
